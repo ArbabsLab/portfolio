a86105cfcd752f51a80a880abf12bccd98c37d5b
Arbab Husain <arbabhusain200@gmail.com>
2025-07-25 02:14:02 -0400

    Updated redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# 1. CD into project folder
 cd /root/portfolio
+
+# 2. Fetch latest code from GitHub
 git fetch
 git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
+# 3. Spin down containers
+docker compose -f docker-compose.prod.yml down
 
-pip install -r requirements.txt
-deactivate
+# 4. Rebuild and spin up containers
+docker compose -f docker-compose.prod.yml up -d --build
 
-systemctl daemon-reload
-systemctl restart myportfolio
-systemctl status myportfolio
+
